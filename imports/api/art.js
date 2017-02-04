cf216fc2a10e62b5b0740f6b1648873d8231bee8
Minyong Cho <minyongcho@Minyongs-MacBook-Air.local>
2017-02-04 17:50:15 -0500

    use update upsert

--- imports/api/art.js
+++ imports/api/art.js
@@ -1,5 +1,8 @@
 import { Mongo } from 'meteor/mongo';
 
+
 export const Artworks = new Mongo.Collection('artworks');
 
-//Artworks.insert({title: "ici-on-creve", body: 'Ici on crève (\"We\'re Dying Here\") Aimé Mpane 2006-2008'});
+// Artworks.remove({title: "ici-on-creve"});
+
+//Artworks.update({title: "ici-on-creve"}, {$addToSet: {body: 'Ici on crève (\"We\'re Dying Here\") Aimé Mpane 2006-2008'}}, {upsert: true} );
