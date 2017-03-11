41b67c00c56cc696e1e03b5f53a9aeff71e04186
Minyong Cho <minyongcho@Minyongs-MacBook-Air.local>
2017-03-11 00:21:48 -0500

    add heat map

--- imports/api/art.js
+++ imports/api/art.js
@@ -4,16 +4,18 @@ import { Mongo } from 'meteor/mongo';
 export const Artworks = new Mongo.Collection('artworks');
 
 //Artworks.remove({title: "ici-on-creve-by-aime-mpane-at-brooklyn-museum-of-art"});
-//Artworks.remove({_id: Artworks.findOne({title: "mimesis-by-dawit-l-petros-at-center-for-photography-woodstock"})._id});
+//Artworks.remove({_id: Artworks.findOne({title: "heat-maps-by-richard-mosse-at-jack-shainman-gallery"})._id});
 
 visitdate1 = Date.parse("December 28, 2016");
 visitdate2 = Date.parse("January 1, 2017");
 visitdate3 = Date.parse("November 15, 2015");
 visitdate4 = Date.parse("November 15, 2016");
+visitdate5 = Date.parse("February 25, 2017");
 
 //Artworks.update({title: "ici-on-creve-by-aime-mpane-at-brooklyn-museum"}, {$addToSet: {pagetitle: "Burnt Identity", visitdate: visitdate1, image1: "ici-on-creve.jpg", image2: "figure-of-mother-and-child.jpg", artworktitle1: 'Ici on crève (\"We\'re Dying Here\")', link1: 'https://www.brooklynmuseum.org/opencollection/objects/210261', artworktitle2: 'Figure of Mother and Child (Phemba)', link2: 'https://www.brooklynmuseum.org/opencollection/objects/2948', artist1: "Aimé Mpane", artist2: 'Kongo people (Yombe subgroup)', date1: "2006-2008", date2: "19th century", landing: "Brooklyn Museum's portraits from Africa ask me questions about my identity."}}, {upsert: true} );
 //Artworks.update({title: "menace-by-zeev-willy-neumann-at-saugerties-ny"}, {$addToSet: {pagetitle: "Home", visitdate: visitdate2, image1: "menace.jpg", artworktitle1: 'Menace', link1: 'http://zwneumann.tumblr.com/menace', artist1: "Ze'ev Willy Neumann", date1: "2014", landing: "There is no place like home"}}, {upsert: true} );
 //Artworks.update({title: "pool-by-jihyeon-lee-at-society-of-illustrators"}, {$addToSet: {pagetitle: "City Whale", visitdate: visitdate3, image1: "pool.jpg", image2: "pool1.jpg", artworktitle1: 'Pool', link1: 'http://blog.picturebookmakers.com/post/125243484756/jihyeon-lee', artist1: "JiHyeon Lee", date1: "2013", landing: "The illustrated pool puts me face to face with a whale in the middle of New York City."}}, {upsert: true} );
 //Artworks.update({title: "mimesis-by-dawit-l-petros-at-center-for-photography-woodstock"}, {$addToSet: {pagetitle: "Barn Becoming", visitdate: visitdate4, image1: "barn-becoming.jpg", artworktitle1: 'Mimesis', link1: 'http://www.dawitlpetros.com/', artist1: "Dawit L Petros", date1: "2007", landing: "I lie with a man under a barn."}}, {upsert: true} );
+//Artworks.update({title: "heat-maps-by-richard-mosse-at-jack-shainman-gallery"}, {$addToSet: {pagetitle: "Refugee in February", visitdate: visitdate5, image1: "heatmap1.jpg", image2: "heatmap2.jpg", artworktitle1: 'Heat Maps (Tempelhof Interior)', link1: 'http://www.richardmosse.com/', artist1: "Richard Mosse", date1: "2016", landing: "The refugee in the Nazi airport connects with me through his body heat."}}, {upsert: true} );
 
 //console.log(Artworks.findOne({title: "ici-on-creve-by-aime-mpane-at-brooklyn-museum-of-art"}));
