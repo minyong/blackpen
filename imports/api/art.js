b2b9b7ecf962fd83ae9415a46a54d6bef57e4a0f
Minyong Cho <minyongcho@Minyongs-MacBook-Air.local>
2017-04-17 22:12:33 -0400

    add mit and dome

--- imports/api/art.js
+++ imports/api/art.js
@@ -6,13 +6,16 @@ export const Artworks = new Mongo.Collection('artworks');
 //Artworks.remove({title: "ici-on-creve-by-aime-mpane-at-brooklyn-museum-of-art"});
 //Artworks.remove({_id: Artworks.findOne({title: "untitled-by-dan-flavin-at-dia-beacon"})._id});
 
-visitdate1 = Date.parse("December 28, 2016");
-visitdate2 = Date.parse("January 1, 2017");
-visitdate3 = Date.parse("November 15, 2015");
-visitdate4 = Date.parse("November 15, 2016");
-visitdate5 = Date.parse("February 25, 2017");
-visitdate6 = Date.parse("March 12, 2017");
-visitdate7 = Date.parse("October 1, 2016");
+// visitdate1 = Date.parse("December 28, 2016");
+// visitdate2 = Date.parse("January 1, 2017");
+// visitdate3 = Date.parse("November 15, 2015");
+// visitdate4 = Date.parse("November 15, 2016");
+// visitdate5 = Date.parse("February 25, 2017");
+// visitdate6 = Date.parse("March 12, 2017");
+// visitdate7 = Date.parse("October 1, 2016");
+//visitdate8 = Date.parse("April 2, 2017");
+//visitdate9 = Date.parse("April 12, 2017");
+visitdate10 = Date.parse("April 17, 2017");
 
 //Artworks.update({title: "ici-on-creve-by-aime-mpane-at-brooklyn-museum"}, {$addToSet: {pagetitle: "Burnt Identity", visitdate: visitdate1, image1: "ici-on-creve.jpg", image2: "figure-of-mother-and-child.jpg", artworktitle1: 'Ici on crève (\"We\'re Dying Here\")', link1: 'https://www.brooklynmuseum.org/opencollection/objects/210261', artworktitle2: 'Figure of Mother and Child (Phemba)', link2: 'https://www.brooklynmuseum.org/opencollection/objects/2948', artist1: "Aimé Mpane", artist2: 'Kongo people (Yombe subgroup)', date1: "2006-2008", date2: "19th century", landing: "Brooklyn Museum's portraits from Africa ask me questions about my identity."}}, {upsert: true} );
 //Artworks.update({title: "menace-by-zeev-willy-neumann-at-saugerties-ny"}, {$addToSet: {pagetitle: "Home", visitdate: visitdate2, image1: "menace.jpg", artworktitle1: 'Menace', link1: 'http://zwneumann.tumblr.com/menace', artist1: "Ze'ev Willy Neumann", date1: "2014", landing: "There is no place like home"}}, {upsert: true} );
@@ -21,5 +24,9 @@ visitdate7 = Date.parse("October 1, 2016");
 //Artworks.update({title: "heat-maps-by-richard-mosse-at-jack-shainman-gallery"}, {$addToSet: {pagetitle: "Refugee in February", visitdate: visitdate5, image1: "heatmap1.jpg", image2: "heatmap2.jpg", artworktitle1: 'Heat Maps (Tempelhof Interior)', link1: 'http://www.richardmosse.com/', artist1: "Richard Mosse", date1: "2016", landing: "The refugee in the Nazi airport connects with me through his body heat."}}, {upsert: true} );
 //Artworks.update({title: "the-old-man-and-the-youth-by-riza-abbasi-at-metropolitan-museum-of-art"}, {$addToSet: {pagetitle: "Lithe Fruiterer", visitdate: visitdate6, image1: "old-man-and-youth.jpg", artworktitle1: 'The Old Man and the Youth', link1: 'http://www.metmuseum.org/art/collection/search/447798', artist1: "Riza Abbasi", date1: "17th century", landing: "I now understand the ubiquitous topos of an old man falling in love with a youth in Persian painting."}}, {upsert: true} );
 //Artworks.update({title: "untitled-by-fred-sandback-at-dia-beacon"}, {$addToSet: {pagetitle: "Spatial Dia", visitdate: visitdate7, image1: "spatial-dia.jpg", artworktitle1: 'Untitled (from Ten Vertical Constructions [rust red variation])', link1: 'http://www.diaart.org/collection/collection/sandback-fred-untitled-from-ten-vertical-constructions-rust-red-variation-197779-2014-003-1-2', artist1: "Fred Sandback, All artwork by Fred Sandback © 2017 Fred Sandback Archive", date1: "1977-79", landing: "Dia:Beacon's monumental artworks define space anew."}}, {upsert: true} );
+//Artworks.update({title: "rsvp-by-senga-nengudi-at-studio-museum"}, {$addToSet: {pagetitle: "Harlem Stockings", visitdate: visitdate8, image1: "harlem-stockings.jpg", artworktitle1: 'R.S.V.P.', link1: 'http://www.diaart.org/collection/collection/sandback-fred-untitled-from-ten-vertical-constructions-rust-red-variation-197779-2014-003-1-2', artist1: "Senga Nengudi", date1: "1976", landing: "Stockings in Harlem brings art historical awakening of black female sculptors of the 60s."}}, {upsert: true} );
+//Artworks.update({title: "mit"}, {$addToSet: {pagetitle: "MIT Mom", visitdate: visitdate9, image1: "mit-mom.jpg", artworktitle1: 'MIT Killian Court', link1: 'https://www.technologyreview.com/s/522526/the-backstory-of-killian-court/', artist1: "William W. Bosworth", date1: "1916", landing: "The MIT architecture was a mother to me when I needed one."}}, {upsert: true} );
+//Artworks.update({title: "dome-of-rock-at-jerusalem"}, {$addToSet: {pagetitle: "Dome of Muhammad", visitdate: visitdate10, image1: "dome-muhammad.jpg", artworktitle1: 'The Dome of the Rock', link1: 'https://archnet.org/sites/2814/publications/3027', artist1: "First Commissioned by Umayyad Caliph Abd al-Malik", date1: "691 CE", landing: "Muhammad and I fall in love at the Dome of the Rock"}}, {upsert: true} );
+
 
 //console.log(Artworks.findOne({title: "ici-on-creve-by-aime-mpane-at-brooklyn-museum-of-art"}));
